83953382e449620c3fe411a33977de1c8baaf15d
Uellington Damasceno <uellington99@gmail.com>
2021-08-18 14:06:46 -0300

    First commit.

---
+++ src/main/java/pokedex/api/PokedexApiApplication.java
@@ -0,0 +1,13 @@
+package pokedex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApiApplication.class, args);
+	}
+
+}
